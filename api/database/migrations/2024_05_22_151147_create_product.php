2e1e3e58715b74d4b9904f72288f370aada80a15
RinRavit <128680924+RinRavit@users.noreply.github.com>
2024-06-05 22:01:20 +0700

    fist commit

---
+++ api/database/migrations/2024_05_22_151147_create_product.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->increments('id_product');
+            $table->unsignedInteger('id_category');
+            $table->string('name_product')->unique();
+            $table->string('brand')->nullable();
+            $table->integer('buy_price');
+            $table->tinyInteger('discount')->default(0);
+            $table->integer('selling_price');
+            $table->integer('stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product');
+    }
+};
